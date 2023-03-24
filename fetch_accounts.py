80d173c1bc54952e8f6a96a49be5ef80bd1f1d87
Ognjen Duricic <ogigoc@gmail.com>
2023-03-24 14:50:49 +0100

    Fetch accounts automatically

--- fetch_accounts.py
+++ fetch_accounts.py
@@ -1,7 +1,12 @@
+#!./env/bin/python
 import pandas as pd
 import gspread
 
+print("Fetching account sheet...")
 gc = gspread.service_account('service_account.json')
 sheet = gc.open("Tiktok Nalozi").sheet1
 accounts = pd.DataFrame(sheet.get_all_records())
+accounts = accounts.applymap(lambda x: True if x == 'TRUE' else False if x == 'FALSE' else x)
 accounts.to_json('accounts_db.json', orient='records', lines=True)
+
+print("Done.")
