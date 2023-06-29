71d765e3e5d5d44071b3c5eff5987b6670b1fa68
tiktune <tiktunesrbija@gmail.com>
2023-06-29 19:11:48 +0200

    Update job and devices

--- job.sh
+++ job.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 [ -z "$1" ] && { echo "Usage: $0 <device>"; exit 1; }
 
-cd /home/ognjen/Projects/tiktok-promo
+cd /home/tiktune/tiktune
 source env/bin/activate
 python main.py -d $1 2>&1 | tee -a logs.txt
